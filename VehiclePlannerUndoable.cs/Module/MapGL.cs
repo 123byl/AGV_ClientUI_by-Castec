69fa2246d821bbdf60af531056121c63f0ca5b72
Tim Kuo <timkuo@castec.com.tw>
2018-08-21 10:39:05 +0800

    加入連線時才開啟部分功能

--- VehiclePlannerUndoable.cs/Module/MapGL.cs
+++ VehiclePlannerUndoable.cs/Module/MapGL.cs
@@ -1,4 +1,5 @@
-﻿using GLStyle;
+﻿using CtLib.Library;
+using GLStyle;
 using GLUI;
 using System;
 using System.Collections.Generic;
@@ -12,33 +13,102 @@ using System.Windows.Forms;
 using VehiclePlanner.Module.Implement;
 using WeifenLuo.WinFormsUI.Docking;
 
-namespace VehiclePlannerUndoable.cs {
 
-    /// <summary>
-    /// 以可重做MapGL進行實作之MapGL介面
-    /// </summary>
-    public partial class MapGL : BaseMapGL ,IMapGL{
+namespace VehiclePlannerUndoable.cs
+{
 
-        private GLUICtrl mMapGL = new GLUICtrl();
+	/// <summary>
+	/// 以可重做MapGL進行實作之MapGL介面
+	/// </summary>
+	public partial class MapGL : BaseMapGL, IMapGL
+	{
 
-        /// <summary>
-        /// 地圖控制項
-        /// </summary>
-        public GLUICtrl MapControl { get => mMapGL; }
+		private GLUICtrl mMapGL = new GLUICtrl();
 
-        protected MapGL() {
-            InitializeComponent();
+		private CtVehiclePlanner_Ctrl ParrentUI { get => rUI as CtVehiclePlanner_Ctrl; }
 
-        }
-        public MapGL(CtVehiclePlanner_Ctrl refUI, DockState defState = DockState.Float) : base(refUI,defState) {
-            InitializeComponent();
+		/// <summary>
+		/// 地圖控制項
+		/// </summary>
+		public GLUICtrl MapControl { get => mMapGL; }
 
-            // 載入設定檔
-            StyleManager.LoadStyle("Style.ini");
-            
-            mMapGL.Location = new Point(0, 0);
-            mMapGL.Dock = DockStyle.Fill;
-            pnlShow.Controls.Add(mMapGL);
-        }
-    }
+		protected MapGL()
+		{
+			InitializeComponent();
+
+		}
+		public MapGL(CtVehiclePlanner_Ctrl refUI, DockState defState = DockState.Float) : base(refUI, defState)
+		{
+			InitializeComponent();
+
+			// 載入設定檔
+			StyleManager.LoadStyle("Style.ini");
+
+			mMapGL.Location = new Point(0, 0);
+			mMapGL.Dock = DockStyle.Fill;
+			pnlShow.Controls.Add(mMapGL);
+		}
+
+		public void ButtonEnable(bool enable)
+		{
+			this.InvokeIfNecessary(() =>
+			{
+				tsbAutoReport.Enabled = enable;
+				tsbChangeMap.Enabled = enable;
+				tsbConfirm.Enabled = enable;
+				tsbController.Enabled = enable;
+				tsbGetLaser.Enabled = enable;
+				tsbGetMap.Enabled = enable;
+				tsbLocalization.Enabled = enable;
+				tsbScan.Enabled = enable;
+				tsbSendMap.Enabled = enable;
+			});
+		}
+
+		protected override void tsbSetCar_Click(object sender, EventArgs e)
+		{
+			ParrentUI.ITest_SettingCarPos();
+			tsbLocalization.Checked = !tsbLocalization.Checked;
+			if (tsbLocalization.Checked)
+			{
+				tsbAutoReport.Enabled = false;
+				tsbChangeMap.Enabled = false;
+				tsbClearMap.Enabled = false;
+				tsbConfirm.Enabled = false;
+				tsbConnect.Enabled = false;
+				tsbController.Enabled = false;
+				tsbGetLaser.Enabled = false;
+				tsbGetMap.Enabled = false;
+				tsbInsertMap.Enabled = false;
+				tsbOpenFile.Enabled = false;
+				tsbSave.Enabled = false;
+				tsbScan.Enabled = false;
+				tsbSendMap.Enabled = false;
+			}
+			else if(!tsbLocalization.Checked && ParrentUI.rVehiclePlanner.Controller.ConnectStatus)
+			{
+				tsbAutoReport.Enabled = true;
+				tsbChangeMap.Enabled = true;
+				tsbClearMap.Enabled = true;
+				tsbConfirm.Enabled = true;
+				tsbConnect.Enabled = true;
+				tsbController.Enabled = true;
+				tsbGetLaser.Enabled = true;
+				tsbGetMap.Enabled = true;
+				tsbInsertMap.Enabled = true;
+				tsbOpenFile.Enabled = true;
+				tsbSave.Enabled = true;
+				tsbScan.Enabled = true;
+				tsbSendMap.Enabled = true;
+			}
+			else
+			{
+				tsbClearMap.Enabled = true;
+				tsbConnect.Enabled = true;
+				tsbInsertMap.Enabled = true;
+				tsbOpenFile.Enabled = true;
+				tsbSave.Enabled = true;
+			}
+		}
+	}
 }
