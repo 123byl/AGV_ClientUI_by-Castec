b076b6957aa644dbd5eff2c1211fc0f39a261d85
Tim Kuo <timkuo@castec.com.tw>
2018-08-30 17:18:28 +0800

    修改介面按鍵機制

--- VehiclePlannerUndoable.cs/Module/MapGL.cs
+++ VehiclePlannerUndoable.cs/Module/MapGL.cs
@@ -24,9 +24,25 @@ namespace VehiclePlannerUndoable.cs
 	/// </summary>
 	public partial class MapGL : BaseMapGL, IMapGL
 	{
+		#region Declaration - Fields
+		private enum EMotion
+		{
+			Location,
+			Movement,
+			Disconnect
+		}
 
+		/// <summary>
+		/// Map sub UI
+		/// </summary>
 		private GLUICtrl mMapGL = new GLUICtrl();
 
+		#endregion
+
+		#region Declaration - Properties
+		/// <summary>
+		/// 父視窗
+		/// </summary>
 		private CtVehiclePlanner_Ctrl ParrentUI { get => rUI as CtVehiclePlanner_Ctrl; }
 
 		/// <summary>
@@ -34,11 +50,16 @@ namespace VehiclePlannerUndoable.cs
 		/// </summary>
 		public GLUICtrl MapControl { get => mMapGL; }
 
+		#endregion
+
+
 		protected MapGL()
 		{
 			InitializeComponent();
 			tsbGetLaser.Visible = false;
 		}
+
+
 		public MapGL(CtVehiclePlanner_Ctrl refUI, DockState defState = DockState.Float) : base(refUI, defState)
 		{
 			InitializeComponent();
@@ -46,6 +67,7 @@ namespace VehiclePlannerUndoable.cs
 			// 載入設定檔
 			StyleManager.LoadStyle("Style.ini");
 			tsbGetLaser.Visible = false;
+			//tsbMove.Visible = false;
 			ConnectButtonEnable(false);
 			mMapGL.Location = new Point(0, 0);
 			mMapGL.Dock = DockStyle.Fill;
@@ -66,6 +88,8 @@ namespace VehiclePlannerUndoable.cs
 				if (!enable) tsbLocalization.Checked = enable;
 				tsbScan.Enabled = enable;
 				tsbSendMap.Enabled = enable;
+				tsbMove.Enabled = enable;
+				if (!enable) tsbMove.Checked = enable;
 				tsbConnect.Image = enable ? Resources.Connect : Resources.Disconnect;
 			});
 		}
@@ -73,48 +97,164 @@ namespace VehiclePlannerUndoable.cs
 		protected override void tsbSetCar_Click(object sender, EventArgs e)
 		{
 			ParrentUI.Locate();
-			tsbLocalization.Checked = !tsbLocalization.Checked;
-			if (tsbLocalization.Checked)
+			ButtonControlEnable(!tsbLocalization.Checked, EMotion.Location);
+			if (!tsbLocalization.Checked) ParrentUI.CancelLocalize();
+			//tsbLocalization.Checked = !tsbLocalization.Checked;
+			//if (tsbLocalization.Checked)
+			//{
+			//	tsbAutoReport.Enabled = false;
+			//	tsbChangeMap.Enabled = false;
+			//	tsbClearMap.Enabled = false;
+			//	tsbConfirm.Enabled = false;
+			//	tsbConnect.Enabled = false;
+			//	tsbController.Enabled = false;
+			//	tsbGetLaser.Enabled = false;
+			//	tsbGetMap.Enabled = false;
+			//	tsbInsertMap.Enabled = false;
+			//	tsbOpenFile.Enabled = false;
+			//	tsbSave.Enabled = false;
+			//	tsbScan.Enabled = false;
+			//	tsbSendMap.Enabled = false;
+			//	tsbMove.Enabled = false;
+			//}
+			//else if (!tsbLocalization.Checked && ParrentUI.rVehiclePlanner.Controller.ConnectStatus)
+			//{
+			//	tsbAutoReport.Enabled = true;
+			//	tsbChangeMap.Enabled = true;
+			//	tsbClearMap.Enabled = true;
+			//	tsbConfirm.Enabled = true;
+			//	tsbConnect.Enabled = true;
+			//	tsbController.Enabled = true;
+			//	tsbGetLaser.Enabled = true;
+			//	tsbGetMap.Enabled = true;
+			//	tsbInsertMap.Enabled = true;
+			//	tsbOpenFile.Enabled = true;
+			//	tsbSave.Enabled = true;
+			//	tsbScan.Enabled = true;
+			//	tsbSendMap.Enabled = true;
+			//	tsbMove.Enabled = true;
+			//	ParrentUI.CancelLocalize();
+			//}
+			//else
+			//{
+			//	tsbClearMap.Enabled = true;
+			//	tsbConnect.Enabled = true;
+			//	tsbInsertMap.Enabled = true;
+			//	tsbOpenFile.Enabled = true;
+			//	tsbSave.Enabled = true;
+			//	ParrentUI.CancelLocalize();
+			//}
+		}
+
+		protected override void tsbMove_Click(object sender, EventArgs e)
+		{
+			ParrentUI.MovePosition();
+			ButtonControlEnable(!tsbMove.Checked, EMotion.Movement);
+			if (!tsbMove.Checked) ParrentUI.CancelMovement();
+			//tsbMove.Checked = !tsbMove.Checked;
+			//if (tsbMove.Checked)
+			//{
+			//	tsbAutoReport.Enabled = false;
+			//	tsbChangeMap.Enabled = false;
+			//	tsbClearMap.Enabled = false;
+			//	tsbConfirm.Enabled = false;
+			//	tsbConnect.Enabled = false;
+			//	tsbController.Enabled = false;
+			//	tsbGetLaser.Enabled = false;
+			//	tsbGetMap.Enabled = false;
+			//	tsbInsertMap.Enabled = false;
+			//	tsbOpenFile.Enabled = false;
+			//	tsbSave.Enabled = false;
+			//	tsbScan.Enabled = false;
+			//	tsbSendMap.Enabled = false;
+			//	tsbLocalization.Enabled = false;
+			//}
+			//else if (!tsbMove.Checked && ParrentUI.rVehiclePlanner.Controller.ConnectStatus)
+			//{
+			//	tsbAutoReport.Enabled = true;
+			//	tsbChangeMap.Enabled = true;
+			//	tsbClearMap.Enabled = true;
+			//	tsbConfirm.Enabled = true;
+			//	tsbConnect.Enabled = true;
+			//	tsbController.Enabled = true;
+			//	tsbGetLaser.Enabled = true;
+			//	tsbGetMap.Enabled = true;
+			//	tsbInsertMap.Enabled = true;
+			//	tsbOpenFile.Enabled = true;
+			//	tsbSave.Enabled = true;
+			//	tsbScan.Enabled = true;
+			//	tsbSendMap.Enabled = true;
+			//	tsbLocalization.Enabled = true;
+			//	ParrentUI.CancelMovement();
+			//}
+			//else
+			//{
+			//	tsbClearMap.Enabled = true;
+			//	tsbConnect.Enabled = true;
+			//	tsbInsertMap.Enabled = true;
+			//	tsbOpenFile.Enabled = true;
+			//	tsbSave.Enabled = true;
+			//	ParrentUI.CancelMovement();
+			//}
+
+		}
+
+		public void MovePositionFinish()
+		{
+			ButtonControlEnable(!tsbMove.Checked, EMotion.Movement);
+			//if (ParrentUI.rVehiclePlanner.Controller.ConnectStatus)
+			//{
+			//	tsbMove.Checked = false;
+			//	tsbAutoReport.Enabled = true;
+			//	tsbChangeMap.Enabled = true;
+			//	tsbClearMap.Enabled = true;
+			//	tsbConfirm.Enabled = true;
+			//	tsbController.Enabled = true;
+			//	tsbGetLaser.Enabled = true;
+			//	tsbGetMap.Enabled = true;
+			//	tsbScan.Enabled = true;
+			//	tsbSendMap.Enabled = true;
+			//	tsbLocalization.Enabled = true;
+			//	tsbMove.Enabled = true;
+			//}
+			//tsbMove.Checked = false;
+			//tsbOpenFile.Enabled = true;
+			//tsbSave.Enabled = true;
+			//tsbClearMap.Enabled = true;
+			//tsbConnect.Enabled = true;
+			//tsbInsertMap.Enabled = true;
+		}
+
+		private void ButtonControlEnable(bool enable, EMotion type)
+		{
+			//enable = ParrentUI.rVehiclePlanner.Controller.ConnectStatus ? enable : true;
+			//type = ParrentUI.rVehiclePlanner.Controller.ConnectStatus ? type : EMotion.Disconnect;
+			tsbOpenFile.Enabled = !enable;
+			tsbSave.Enabled = !enable;
+			tsbClearMap.Enabled = !enable;
+			tsbConnect.Enabled = !enable;
+			tsbInsertMap.Enabled = !enable;
+			tsbGetMap.Enabled = !enable;
+			tsbSendMap.Enabled = !enable;
+			tsbChangeMap.Enabled = !enable;
+			tsbScan.Enabled = !enable;
+			tsbController.Enabled = !enable;
+			tsbAutoReport.Enabled = !enable;
+			tsbConfirm.Enabled = !enable;
+			switch (type)
 			{
-				tsbAutoReport.Enabled = false;
-				tsbChangeMap.Enabled = false;
-				tsbClearMap.Enabled = false;
-				tsbConfirm.Enabled = false;
-				tsbConnect.Enabled = false;
-				tsbController.Enabled = false;
-				tsbGetLaser.Enabled = false;
-				tsbGetMap.Enabled = false;
-				tsbInsertMap.Enabled = false;
-				tsbOpenFile.Enabled = false;
-				tsbSave.Enabled = false;
-				tsbScan.Enabled = false;
-				tsbSendMap.Enabled = false;
-			}
-			else if(!tsbLocalization.Checked && ParrentUI.rVehiclePlanner.Controller.ConnectStatus)
-			{
-				tsbAutoReport.Enabled = true;
-				tsbChangeMap.Enabled = true;
-				tsbClearMap.Enabled = true;
-				tsbConfirm.Enabled = true;
-				tsbConnect.Enabled = true;
-				tsbController.Enabled = true;
-				tsbGetLaser.Enabled = true;
-				tsbGetMap.Enabled = true;
-				tsbInsertMap.Enabled = true;
-				tsbOpenFile.Enabled = true;
-				tsbSave.Enabled = true;
-				tsbScan.Enabled = true;
-				tsbSendMap.Enabled = true;
-				ParrentUI.CancelLocalize();
-			}
-			else
-			{
-				tsbClearMap.Enabled = true;
-				tsbConnect.Enabled = true;
-				tsbInsertMap.Enabled = true;
-				tsbOpenFile.Enabled = true;
-				tsbSave.Enabled = true;
-				ParrentUI.CancelLocalize();
+				case EMotion.Location:
+					tsbLocalization.Checked = enable;
+					tsbMove.Checked = !enable ? false : !enable;
+					break;
+				case EMotion.Movement:
+					tsbLocalization.Checked = !enable ? false : !enable;
+					tsbMove.Checked = enable;
+					break;
+				case EMotion.Disconnect:
+					tsbLocalization.Checked = enable;
+					tsbMove.Checked = enable;
+					break;
 			}
 		}
 	}
